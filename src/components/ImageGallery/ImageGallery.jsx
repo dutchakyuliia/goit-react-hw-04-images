24d0b7102d6143b1276c5a43e77dc9302a9a9358
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-05-23 22:23:41 +0200

    fix build deploy

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { getImages } from 'services/getImages';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
